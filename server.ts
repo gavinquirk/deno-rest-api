91c98a75bcc9d865f9a7a082e184be211b251874
Gavin Quirk <gavinquirk@gmail.com>
2020-05-20 17:05:36 -0700

    separate controller and routes files, interface types

--- server.ts
+++ server.ts
@@ -1,19 +1,14 @@
 // Oak is a middleware framework for Deno's http server
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
 
 const port = 5000;
 
 const app = new Application();
 
-// Enable router
-const router = new Router();
 app.use(router.routes());
 // Allow methods -- can be restricted
 app.use(router.allowedMethods());
-
-router.get("/api/v1/products", ({ response }: { response: any }) => {
-  response.body = "Hello World";
-});
 
 console.log(`Server running on port ${port}`);
 
