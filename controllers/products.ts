42a5f8a762fb391fdccb6baa8d52b4d6386ada49
Gavin Quirk <gavinquirk@gmail.com>
2020-06-09 17:23:45 -0700

    getProduct method

--- controllers/products.ts
+++ controllers/products.ts
@@ -65,17 +65,46 @@ const getProducts = async ({ response }: { response: any }) => {
 
 // @desc    Get single product
 // @route   GET /api/v1/products/:id
-const getProduct = (
+const getProduct = async (
   { response, params }: { response: any; params: { id: string } },
 ) => {
-  const product: Product | undefined = products.find((p) => p.id === params.id);
+  try {
+    await client.connect();
 
-  if (product) {
-    response.status = 200;
-    response.body = { success: true, data: product };
-  } else {
-    response.status = 404;
-    response.body = { success: false, msg: "No product found with that id" };
+    const result = await client.query(
+      "SELECT * FROM products WHERE id = $1",
+      params.id,
+    );
+
+    if (result.rows.toString() === "") {
+      response.status = 404;
+      response.body = {
+        success: false,
+        msg: `No product with the id of ${params.id}`,
+      };
+      return;
+    } else {
+      const product: any = new Object();
+
+      result.rows.map((p) => {
+        result.rowDescription.columns.map((el, i) => {
+          product[el.name] = p[i];
+        });
+      });
+
+      response.body = {
+        success: true,
+        data: product,
+      };
+    }
+  } catch (error) {
+    response.status = 500;
+    response.body = {
+      success: false,
+      msg: error.toString(),
+    };
+  } finally {
+    await client.end();
   }
 };
 
