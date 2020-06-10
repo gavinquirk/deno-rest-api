d31271fb36602e1a672c29bc0ad9e6dda950c4c1
Gavin Quirk <gavinquirk@gmail.com>
2020-06-09 17:32:45 -0700

    updateProduct method

--- controllers/products.ts
+++ controllers/products.ts
@@ -158,26 +158,52 @@ const updateProduct = async (
     params: { id: string };
   },
 ) => {
-  const product: Product | undefined = products.find((p) => p.id === params.id);
+  await getProduct({ params: { "id": params.id }, response });
 
-  if (product) {
-    const body = await request.body();
-
-    const updateData: { name?: string; description?: string; price?: number } =
-      body.value;
-
-    products = products.map((p) =>
-      p.id === params.id ? { ...p, ...updateData } : p
-    );
-
-    response.status = 200;
+  if (response.status === 404) {
     response.body = {
-      success: true,
-      data: products,
+      success: false,
+      msg: response.body.msg,
     };
-  } else {
+
     response.status = 404;
-    response.body = { success: false, msg: "No product found with that id" };
+    return;
+  } else {
+    const body = await request.body();
+    const product = body.value;
+
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: "No data",
+      };
+    } else {
+      try {
+        await client.connect();
+        const result = await client.query(
+          "UPDATE products SET name=$1, description=$2, price=$3 WHERE id=$4",
+          product.name,
+          product.description,
+          product.price,
+          params.id,
+        );
+
+        response.status = 200;
+        response.body = {
+          success: true,
+          data: product,
+        };
+      } catch (error) {
+        response.status = 500;
+        response.body = {
+          success: false,
+          msg: error.toString(),
+        };
+      } finally {
+        await client.end();
+      }
+    }
   }
 };
 
